acee2c9f1d036d9971ec0acbd59343b52576130c
mpawel1993 <mpawel1993@gmail.com>
2026-09-16 15:05:34 +0200

    test

--- test.ts
+++ test.ts
@@ -72,3 +72,26 @@ export default defineConfig({
     },
   ],
 });
+
+export default defineConfig({
+  projects: [
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        headless: true,
+        launchOptions: {
+          executablePath: '/usr/bin/firefox',
+          // 1. Przekazanie flagi wyłączającej sandbox do Firefoxa
+          args: ['--no-sandbox'],
+          // 2. Wyłączenie piaskownicy przez zmienne środowiskowe procesów Firefoxa
+          env: {
+            ...process.env,
+            MOZ_DISABLE_CONTENT_SANDBOX: '1',
+            MOZ_DISABLE_RDD_SANDBOX: '1',
+          },
+        },
+      },
+    },
+  ],
+});
