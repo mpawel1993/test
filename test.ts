65accb320fba6a7d83c2bc6282e1f977d5b1f266
mpawel1993 <mpawel1993@gmail.com>
2026-09-18 14:39:12 +0200

    test

--- test.ts
+++ test.ts
@@ -13,3 +13,23 @@ test.describe('Weryfikacja wyszukiwarki Google', () => {
     });
 
 });
+
+
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    headless: true,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
